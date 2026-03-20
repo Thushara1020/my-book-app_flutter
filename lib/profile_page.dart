8aef2b499e8a687f13c57b826be2a1e21ed0f6be
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-03-20 09:10:10 +0530

    Update profile_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'database_helper.dart';
 
 class ProfilePage extends StatefulWidget {
